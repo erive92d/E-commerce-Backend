2bd051bc422f8ae946074a0a8129272e46359dcb
Deorren Erive <erive92d@gmail.com>
2023-01-29 19:39:12 -0800

    added model tables and first two routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+})
 
 // Products belongToMany Tags (through ProductTag)
+// Product.belongsToMany(ProductTag,{
+//   foreignKey: "product_id",
+//   onDelete: 'CASCADE'
+// })
 
-// Tags belongToMany Products (through ProductTag)
+// // Tags belongToMany Products (through ProductTag)
+// Tag.belongsToMany(Product, {
+//   foreignKey: "product_id",
+//   onDelete: 'CASCADE'
+// })
 
 module.exports = {
   Product,
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,12 +3,30 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{model: Product}]
+    })
+    res.status(200).json(categoryData)
+  }
+  catch(err){
+    res.status(500).json(err)
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const catData = await Category.findByPk(req.params.id, {
+      include: [{model: Product}]
+    })
+    res.status(200).json(catData)
+  }
+  catch (err) {
+    res.status(500).json(err)
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
